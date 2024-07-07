4ac5ae2d1373e710e1086bedd3f63006f3a9d71e
Agustin Ranieri <aguseranieri@gmail.com>
2024-07-07 16:06:21 -0300

    Nunca debí dudar del paradigma funcional

--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,27 @@ import env from './environment'
 const app = new Hono()
 
 app.get('/', async (c) => {
-  const data = await getData();
+  const table = await getData();
 
   if (!await cache.existsData()) {
     console.log('Cache not found');
-    await cache.saveData(data);
-
+    await cache.saveData(table);
     console.log('Cache created');
-    return c.json(data);
-  }
-
-  if (!await cache.hasChanged(data)) {
+  } else if (await cache.hasChanged(table)) {
+    console.log('Changes found');
+    await sendNotification(table);
+    console.log('Notification sent');
+    await cache.saveData(table);
+  } else {
     console.log('No changes found');
-    return c.json(data);
   }
 
-  console.log('Changes found');
-
-  await sendNotification(data);
-
-  console.log('Notification sent');
-
-  await cache.saveData(data);
-
-  return c.json(data);
+  return c.json(table.map(
+    row => row.reduce(
+      (acc, cell, index) => ({ ...acc, [env.COLUMN_NAMES[index]]: cell }),
+      {}
+    )
+  ));
 });
 
 export default {
